427a90415ab1b1a897614caff97d2db58c67822b
thomasabishop <tactonbishop@gmail.com>
2021-03-06 21:31:09 +0000

    Initial commit

---
+++ src/components/Commits.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Commits.scss';
+function Commits() {
+  return (
+    <React.Fragment>
+      <div className="commits">Another test</div>
+    </React.Fragment>
+  );
+}
+
+export default Commits;
