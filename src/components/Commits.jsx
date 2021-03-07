aa6995bcc335e85997dfade6aa784bb2409b5c25
thomasabishop <tactonbishop@gmail.com>
2021-03-07 13:08:34 +0000

    Create grid, coloration and import filler data for Commits pictograph

--- src/components/Commits.jsx
+++ src/components/Commits.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Commits.scss';
+import { generateYear } from './commitsData';
+
 function Commits() {
+  const data = generateYear();
+  console.log(data);
   return (
-    <React.Fragment>
-      <div className="commits">Another test</div>
-    </React.Fragment>
+    <div className="Commit">
+      <div className="year">
+        {data.map((items, index) => {
+          return (
+            <div key={index} className="week">
+              {items.map((subItems, index) => {
+                return (
+                  <div className={`day-color-${subItems}`} key={index}></div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
