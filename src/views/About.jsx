ee9e677e829059e3e5074d9beb4c7539284bf028
thomasabishop <tactonbishop@gmail.com>
2021-03-29 17:48:01 +0100

    Add copy to About page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
 
 function About() {
-  return <p>This is the About page.</p>;
+  return (
+    <React.Fragment>
+      <p>
+        CodeStats is built with{' '}
+        <a className="about-links" href="https://reactjs.org">
+          React.js
+        </a>{' '}
+        and hosted with AWS Amplify.
+      </p>
+      <p>
+        The data is dynamic and is sourced from the{' '}
+        <a className="about-links" href="https://wakatime.com/">
+          WakaTime
+        </a>{' '}
+        and{' '}
+        <a
+          className="about-links"
+          href="https://docs.github.com/en/rest/reference/users"
+        >
+          GitHub User
+        </a>{' '}
+        APIs. WakaTime is a software that hooks into your text editor or IDE and
+        records metrics about your coding over time.
+      </p>
+      <p>
+        The source code is available{' '}
+        <a
+          className="about-links"
+          href="https://github.com/thomasabishop/code-stats"
+        >
+          here
+        </a>
+        .
+      </p>
+
+      <p>
+        Built by{' '}
+        <a className="about-links" href="https://thomas-bishop.co.uk/">
+          Thomas Bishop
+        </a>
+        .
+      </p>
+    </React.Fragment>
+  );
 }
 
 export default About;
